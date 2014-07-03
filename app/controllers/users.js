c5eba1f36e7c5cecbdb7cba0a338ee62313252c3
Paulo McNally <paulomcnally@gmail.com>
2014-07-03 10:25:08 -0600

    auth method added

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,5 +1,23 @@
 var Users = function( models, helpers ){
 
+    this.auth = function( req, res ){
+
+      var email = req.body.email;
+      var password = req.body.password;
+
+      models.user.findOne({ email: email, password: password }, function(error,user){
+
+        if( user ){
+          res.json({ status: true, user: user });
+        }
+        else{
+          res.json({ status: false, message: 'Datos incorrectos. Intenta nuevamente.' });
+        }
+
+      });
+
+    }
+
     this.add = function( req, res ){
 
         var email = req.body.email;
--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,8 @@ var controllers = require('../app/controllers');
 
 exports.init = function( app ){
 
+    app.post( '/auth', controllers.users.auth );
+
     app.post( '/add', controllers.users.add );
 
 }
