f6c2e57ab94d156ab710fcb9a1b459831a033ca2
Paulo McNally <paulomcnally@gmail.com>
2014-07-03 16:58:17 -0600

    fix route model 2

--- app/models/route.js
+++ app/models/route.js
@@ -3,14 +3,8 @@ exports.setup = function(_mongoose,_db){
     var file_name = require('path').basename( __filename, '.js' );
 
     var schema = _mongoose.Schema({
-        name: {
-            type: String
-            }
-        },
-        station: {
-            type: String
-            }
-        },
+        name: String,
+        station: String,
         created_at: {
             type: Date,
             default: Date.now
