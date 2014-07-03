58644c3c51b76e2dc1df010eca2a7f5bffcad053
Paulo McNally <paulomcnally@gmail.com>
2014-07-03 17:06:40 -0600

    remove un used method

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,9 @@ var controllers = require('../app/controllers');
 
 exports.init = function( app ){
 
-    app.post( '/auth', controllers.users.auth );
+    app.post( '/user/auth', controllers.users.auth );
 
-    app.post( '/add', controllers.users.add );
+    app.post( '/user/add', controllers.users.add );
 
     app.post( '/route/add', controllers.route.add );
 
